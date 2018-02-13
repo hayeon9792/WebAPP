cc883dfcb750e25afe3a411a2056c42b88774955
yeonzel <hayeon9792@gmail.com>
2018-02-13 16:56:54 +0900

    php 연습

---
+++ first_project_180210/phpjs/3.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <script charset="utf-8">
+      document.write("Hello world");
+    </script>
+    <?php
+  </body>
+</html>
---
+++ first_project_180210/phpjs/4.php
@@ -0,0 +1,20 @@
+<?php
+$name = "yeonzel"
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>javascript</h1>
+    <script charset="utf-8">
+      name = "hayeon";
+      document.write("안녕하세요, 제 이름은 "+ name +"입니다.");
+    </script>
+    <h1>PHP</h1>
+    <?php
+      echo "안녕하세요. ".$name."님";
+     ?>
+  </body>
+</html>
