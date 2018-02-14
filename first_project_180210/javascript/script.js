42feacb0089dd46fa537d2641f4e8113087ddbf0
yeonzel <hayeon9792@gmail.com>
2018-02-14 17:07:47 +0900

    Revert "button action js파일로 분리"
    This reverts commit 746504a2efa484bfe2233f1ea45fac43da0d6640.

--- first_project_180210/javascript/script.js
+++
@@ -1,9 +0,0 @@
-wbtn = document.getElementById('white_btn');
-wbtn.addEventListener('click', function(){
-  document.getElementById('target').className='white';
-})
-
-bbtn = document.getElementById('blue_btn');
-bbtn.addEventListener('click', function(){
-  document.getElementById('target').className='blue';
-})
