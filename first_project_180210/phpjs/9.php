53aad0e6f0a74ab0ef8cfaf48a7b8465176ed5c4
yeonzel <hayeon9792@gmail.com>
2018-02-14 14:52:24 +0900

    함수 연습

---
+++ first_project_180210/phpjs/9.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>JavaScript</h1>
+    <script>
+      function f(abc) {
+        document.write("Hellooo"+abc);
+      }
+      f("hayeon");
+
+      function add(a,b) {
+        return a+b;
+      }
+      document.write(add(1,5));
+    </script>
+    <h1>PHP</h1>
+    <?php
+      function a($input) {
+        echo "Hello!!".$input;
+      }
+      a("hayeon");
+    ?>
+  </body>
+</html>
