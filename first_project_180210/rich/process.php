0299dc74dc38e0ffa6d0b8a6cc6c173678593bc3
yeonzel <hayeon9792@gmail.com>
2018-02-17 19:43:57 +0900

    no message

--- first_project_180210/rich/process.php
+++ first_project_180210/rich/process.php
@@ -4,5 +4,5 @@
   $sql = "INSERT INTO homepage (title, description, author, created) VALUES ('".$_POST['title']."','".$_POST['description']."','".$_POST['author']."',now())";
   $res = mysqli_query($conn, $sql);
 
-  header('Location: http://localhost/web_project/first_project_180210/html/index.php');
+  header('Location: http://localhost/web_project/first_project_180210/rich/index.php');
 ?>
