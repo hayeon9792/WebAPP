f41b5bc9900ab35cc410e19c9cca90dc27a37bdc
yeonzel <hayeon9792@gmail.com>
2018-02-19 19:42:47 +0900

    name=jin&password=123456' or '1'='1 로 로그인하기

--- first_project_180210/phpjs/14.php
+++ first_project_180210/phpjs/14.php
@@ -2,9 +2,9 @@
   $conn = mysqli_connect('localhost','root','gkdus9792'); // MySQL 접속
   mysqli_select_db($conn, 'webrich'); // DB선택
   $sql = "SELECT * FROM user WHERE name='".$_GET['name']."' AND password='".$_GET['password']."'";
-  //echo "확인용 sql문: ".$sql;
+  echo "확인용 sql문: ".$sql;
   $res = mysqli_query($conn, $sql); //topic 테이블 가져옴
-  //echo "<br/>";
+  echo "<br/>";
   //var_dump($res->num_rows);
 ?>
 
@@ -15,10 +15,10 @@
   </head>
   <body>
     <?php
-      if($res->num_rows) {
-        echo "로그인 되었습니다.";
-      } else {
+      if($res->num_rows == "0") {
         echo "아이디 혹은 비밀번호를 확인해주세요.";
+      } else {
+        echo "로그인 되었습니다.";
       }
      ?>
   </body>
